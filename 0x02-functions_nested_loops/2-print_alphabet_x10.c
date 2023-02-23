41b842209c646b847b4afdaa7ac6526e273495e6
Travisdark <cdakorah@gmail.com>
2023-02-22 16:11:11 -0800

    2-print_alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,18 @@
 
 void print_alphabet_x10(void)
 {
-	char c;
+	char ch;
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
-
-	_putchar('\n');
-	i++;
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
 	}
 }
